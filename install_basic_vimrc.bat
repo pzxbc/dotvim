6b2cabdd0f78692f43dced1b07808d620d767de8
pzxbc <pzxbc@qq.com>
2018-08-14 10:48:41 +0800

    simplify config, just for linux

--- install_basic_vimrc.bat
+++
@@ -1,3 +0,0 @@
-rem 在用户主目录下创建.vimrc文件
-mklink /H %USERPROFILE%\.vimrc vimrc\basic.vim
-echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
--- install_basic_vimrc.sh
+++
@@ -1,2 +0,0 @@
-ln -s ~/.vim_runtime/vimrc/basic.vim > ~/.vimrc
-echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
